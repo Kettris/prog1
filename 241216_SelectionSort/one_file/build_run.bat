003511d724033cfa4813ee0cd99fef07c76c368c
Катя <kattek385@gmail.com>
2024-12-17 02:00:37 +0300

    Create SelectionSort app

--- 241216_SelectionSort/one_file/build_run.bat
+++ 241216_SelectionSort/one_file/build_run.bat
@@ -2,8 +2,8 @@
 chcp 1251 > log
 del log
 
-set MAIN=main.cpp
-set EXE=bubble_sort.exe
+set MAIN=SelectionSort.cpp
+set EXE=SelectionSort.exe
 set CHARSET="-finput-charset=utf-8 -fexec-charset=windows-1251"
 
 if exist %EXE% del %EXE%
