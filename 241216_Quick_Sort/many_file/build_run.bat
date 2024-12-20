c6e8be7fcd92b072f95fbcfa5e93bd377766150b
Катя <kattek385@gmail.com>
2024-12-21 02:17:09 +0300

    Create Quick_Sort app

--- 241216_Quick_Sort/many_file/build_run.bat
+++ 241216_Quick_Sort/many_file/build_run.bat
@@ -2,8 +2,8 @@
 chcp 1251 > log
 del log
 
-set MAIN=SelectionSort.cpp
-set EXE=SelectionSort.exe
+set MAIN="quick_sort.cpp main.cpp"
+set EXE=Quick_Sort.exe
 set CHARSET="-finput-charset=utf-8 -fexec-charset=windows-1251"
 
 if exist %EXE% del %EXE%
