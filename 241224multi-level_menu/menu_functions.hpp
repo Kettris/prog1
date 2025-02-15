45993c5c5d4f5e978b7ff6855a6030b8ecc07ae5
Катя <kattek385@gmail.com>
2025-02-15 13:37:32 +0300

    Start app multi-level_menu

---
+++ 241224multi-level_menu/menu_functions.hpp
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "menu.hpp"
+
+namespace Kate {
+	const MenuItem* read(const MenuItem* current);
+	const MenuItem* exit(const MenuItem* current);
+	
+	const MenuItem* pushkin(const MenuItem* current);
+	const MenuItem* lermontov(const MenuItem* current);
+	const MenuItem* krylov(const MenuItem* current);
+	const MenuItem* main_menu(const MenuItem* current);
+}
