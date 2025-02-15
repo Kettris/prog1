027d9e0b55b711716bcca09c618d0e9df0ef00b7
Катя <kattek385@gmail.com>
2025-02-15 14:58:56 +0300

    Create multi level menu

---
+++ 241224_multi_level_menu/menu_functions.cpp
@@ -0,0 +1,42 @@
+﻿#include "menu_functions.hpp"
+
+#include <cstdlib>
+#include <iostream>
+
+
+const Kate::MenuItem* Kate::read(const MenuItem* current) {
+	for (int i = 1; i < current->children_count; ++i){
+		std::cout << current->children[i]->title << std::endl;	           
+	}
+	std::cout << current->children[0]->title << std::endl;
+	
+	int user_input;
+	std::cin >> user_input;
+	std::cout << std::endl;
+	
+	return current->children[user_input];
+}
+
+const Kate::MenuItem* Kate::exit(const MenuItem* current) {
+	std::exit(0);
+}
+
+const Kate::MenuItem* Kate::pushkin(const MenuItem* current){
+	std::cout << current->title << std::endl << std::endl;
+	return current->parent;
+}
+
+const Kate::MenuItem* Kate::lermontov(const MenuItem* current){
+	std::cout << current->title << std::endl << std::endl;
+	return current->parent;
+}
+
+const Kate::MenuItem* Kate::krylov(const MenuItem* current){
+	std::cout << current->title << std::endl << std::endl;
+	return current->parent;
+}
+
+const Kate::MenuItem* Kate::main_menu(const MenuItem* current){
+	std::cout << current->title << std::endl << std::endl;
+	return current->parent->parent;
+}
